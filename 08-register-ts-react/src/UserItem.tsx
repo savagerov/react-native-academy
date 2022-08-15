b35268d68b3673affc753589de42a81a040cc23c
Sava Gerov <sava.gerov@Savas-Mac-mini.local>
2022-08-15 18:12:07 +0300

    user reg demo added forms

--- 08-register-ts-react/src/UserItem.tsx
+++ 08-register-ts-react/src/UserItem.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
 import { User, UserStatus, UserRole } from "./user.model"
 import { UserListener } from "./UserApp";
@@ -7,9 +8,10 @@ interface UserItemProps {
     user: User;
     onUpdate: UserListener;
     onDelete: UserListener;
+    onEdit: UserListener;
 }
 
-const UserItem = ({user, onUpdate , onDelete}: UserItemProps) => {
+const UserItem = ({user, onUpdate , onDelete, onEdit}: UserItemProps) => {
     function handleCompletion(event: React.MouseEvent) {
         onUpdate({...user, status: UserStatus.SUSPENDED})
     }
@@ -23,18 +25,20 @@ const UserItem = ({user, onUpdate , onDelete}: UserItemProps) => {
             <span className="UserItem-text">
                 <span className="UserItem-id">{user.id}</span>
                 {user.firstName} {user.lastName} - {user.username} - {user.password} {user.gender}
-                {/* <img className="UserItem-id" src={user?.userPicture} alt="Profile picture" /> */}
+                <img className="UserItem-id" src={user?.userPicture} alt="Profile picture" />
             </span>
             <span className="UserItem-right">
-                {/* <span className="UserItem-status">{UserStatus[user.status]}</span> */}
-                {/* {user.status === UserStatus.Active ?
+                <span className="UserItem-status">{UserStatus[user.status]}</span>
+                {user.status === UserStatus.ACTIVE ?
                     (<span className='Btn-first'><span className="UserItem-button fas fa-check-circle"
                         onClick={handleCompletion}></span>
                     <span className="UserItem-button fas fa-times-circle danger"
                         onClick={handleCancel}></span></span> ):
                     <span className="UserItem-button fas fa-times-circle danger"
                         onClick={() => onDelete(user)}></span>
-                } */}
+                } 
+                 <span className="TodoItem-button fas fa-pen-to-square"
+                    onClick={() => onEdit(user)}></span>
             </span>
         </div >
     )
